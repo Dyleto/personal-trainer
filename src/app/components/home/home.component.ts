08684ccb6ac20a033ee0c659fe1c936a0274249a
Dyleto <corentinlemoullec@gmail.com>
2024-07-16 23:10:03 +0200

    session card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { SessionCardComponent } from '../session-card/session-card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrl: 'home.component.scss',
-  imports: [],
+  imports: [SessionCardComponent],
   standalone: true,
 })
 export class HomeComponent {}
---
+++ src/app/components/session-card/session-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-card',
+  templateUrl: 'session-card.component.html',
+  styleUrl: 'session-card.component.scss',
+  standalone: true,
+})
+export class SessionCardComponent {}
