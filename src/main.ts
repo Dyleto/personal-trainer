bba050886204e027c831d0b5c7e279e822f739fe
Dyleto <corentinlemoullec@gmail.com>
2024-07-20 12:55:01 +0200

    add polyfill

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+
+if (
+  !('animate' in document.documentElement) ||
+  (navigator && /iPhone OS (8|9|10|11|12|13)_/.test(navigator.userAgent))
+) {
+  const script = document.createElement('script');
+  script.src = 'web-animations-js.js';
+  document.head.appendChild(script);
+}
